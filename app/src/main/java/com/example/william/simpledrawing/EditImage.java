ac1529ab8069efa3e66d3e97c947d1f07d6cc50b
William <williammarktaylor@gmail.com>
2018-10-04 11:46:42 +1300

    Began work on color picker

--- app/src/main/java/com/example/william/simpledrawing/EditImage.java
+++ app/src/main/java/com/example/william/simpledrawing/EditImage.java
@@ -1,5 +1,14 @@
 package com.example.william.simpledrawing;
 
-public class EditImage
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class EditImage extends AppCompatActivity
 {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.color_picker);
+    }
 }
